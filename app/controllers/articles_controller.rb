c8bafd4e12fa5c2e85932520bfe0d71c1a839387
Alex Williams <alex_williams@protonmail.com>
2019-12-14 17:50:47 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,5 +19,17 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.update(article_params[:id],article_params['article'])
+    redirect_to article_path(@article)
+  end
+
+  private
+  def article_params
+    params.permit('id',{'article' => ['title','description']})
+  end
 end
